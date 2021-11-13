c6374a369b070951779587067bb01cdb38b39f0b
Aldo Ivan Sanchez Leon <aldo.ivan.leon@ibm.com>
2021-11-13 10:33:04 -0600

    fist react keeper

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from './header';
+import Footer from './Footer';
+import Note from './Note';
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
